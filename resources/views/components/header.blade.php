194aafa8fd7310c4796f39181b559e08e76c7d55
Stephen Alonzo <stephena0228@gmail.com>
2025-07-20 22:38:32 +1000

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -19,9 +19,20 @@
                         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">HireMe</span>
                     </a>
                     <div class="flex items-center lg:order-2">
-                        <a href="/post-job/"
-                            class="bg-gray-200 text-gray-500 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Post
-                            a Job</a>
+                        @auth
+                            <span class="text-sm mr-3">Hi, {{ auth()->user()->name ?? false }}!</span>
+                            <form action="/logout" method="get">
+                                @csrf
+                                <button type="submit"
+                                    class="text-gray-700 bg-gray-200 rounded-md text-sm px-4 lg:px-5 py-2 lg:py-2.5 hidden lg:block hover:cursor-pointer">Log
+                                    Out</button>
+                            </form>
+                        @endauth
+                        @guest
+                            <a href="/login"
+                                class="bg-gray-800 text-white dark:text-white hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log
+                                In</a>
+                        @endguest
                         <button data-collapse-toggle="mobile-menu-2" type="button"
                             class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                             aria-controls="mobile-menu-2" aria-expanded="false">
@@ -51,19 +62,12 @@
                                 <a href="/companies"
                                     class="block p-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Companies</a>
                             </li>
-                            @guest
-                                <li>
-                                    <a href="/login"
-                                        class="block p-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Log
-                                        In</a>
-                                </li>
-                            @endguest
                             @auth
                                 <li>
                                     <a href="/applications"
                                         class="block p-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Applications</a>
                                 </li>
-                                <form action="/logout" method="get">
+                                <form action="/logout" method="get" class="lg:hidden">
                                     @csrf
                                     <li>
                                         <button type="submit"
